fc9e9643a6d6507186c1a11d4e3005de4d90d007
katesereda1808 <katesereda22@gmail.com>
2023-01-09 17:02:21 +0300

    fix: add condition to adding eventListener to the loupe

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const close_menu_btn = document.querySelector(".mobile_menu__close_btn");
 const searchbar = document.querySelector(".searchbar__input");
 const loupe = document.querySelector(".loupe");
 
-loupe.addEventListener("click", () => toggleShowElement(searchbar));
+if(loupe){
+    loupe.addEventListener("click", () => toggleShowElement(searchbar));
+}
 menu_btn.addEventListener("click", ()=>toggleShowElement(mobile_menu));
 close_menu_btn.addEventListener("click", () => toggleShowElement(mobile_menu));
 

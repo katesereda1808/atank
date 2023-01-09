f460719a0044db6aa245632b22e98d8a4ea53751
katesereda1808 <katesereda22@gmail.com>
2023-01-09 16:56:57 +0300

    fix: fix circle heading in nlogpost

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const menu_btn = document.querySelector('.burger_menu');
 const mobile_menu = document.querySelector('.mobile_menu');
 const close_menu_btn = document.querySelector(".mobile_menu__close_btn");
+const searchbar = document.querySelector(".searchbar__input");
+const loupe = document.querySelector(".loupe");
 
-menu_btn.addEventListener("click", showMenu);
-close_menu_btn.addEventListener("click", hideMenu);
+loupe.addEventListener("click", () => toggleShowElement(searchbar));
+menu_btn.addEventListener("click", ()=>toggleShowElement(mobile_menu));
+close_menu_btn.addEventListener("click", () => toggleShowElement(mobile_menu));
 
-function showMenu() {
-    console.log('show')
-    mobile_menu.classList.remove('hide');
-}
-function hideMenu() {
-    mobile_menu.classList.add('hide')
+function toggleShowElement(element) {
+    element.classList.toggle('hide')
 }
